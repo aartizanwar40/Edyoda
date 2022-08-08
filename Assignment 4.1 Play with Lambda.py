fca987c0740884397aebf68059eb9212f6635b56
aartizanwar40 <63449239+aartizanwar40@users.noreply.github.com>
2022-08-07 23:31:03 -0700

    Add files via upload

---
+++ Assignment 4.1 Play with Lambda.py
@@ -0,0 +1,3 @@
+number= lambda x : x + 25         
+n = int(input("Enter any number of your choice :- "))   
+print(number(n))
---
+++ Assignment 4.3 Find the squares of given list.py
@@ -0,0 +1,5 @@
+nums = [4,5,2,9] 
+print("Sample list: ", nums)
+result = map(lambda x: x * x, nums) 
+print("\nSquare the element of list numbers:")
+print(list(result))
